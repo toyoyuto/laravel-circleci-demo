19bb9fdaa89876f28226190c8bfe0eff2473232b
toyoyuto <toyoyuto618@gmail.com>
2021-02-06 09:10:12 +0900

    test4

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -12,17 +12,30 @@ class UserRegisterTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * @test
+     * A basic feature test example.
+     *
+     * @return void
      */
-    public function ユーザー登録できる()
+    public function testExample()
     {
-        $email = 'email@example.com';
-        $this->post(route('register'), [
-            'name' => 'user',
-            'email' => $email,
-            'password' => 'password',
-            'password_confirmation' => 'password'
-        ])->assertStatus(302);
-        $this->assertDatabaseHas('users', ['email' => $email]);
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
     }
+
+
+    // /**
+    //  * @test
+    //  */
+    // public function ユーザー登録できる()
+    // {
+    //     $email = 'email@example.com';
+    //     $this->post(route('register'), [
+    //         'name' => 'user',
+    //         'email' => $email,
+    //         'password' => 'password',
+    //         'password_confirmation' => 'password'
+    //     ])->assertStatus(302);
+    //     $this->assertDatabaseHas('users', ['email' => $email]);
+    // }
 }
